444592437d2e4a248511673d00ff1eb975cb40ec
Harry Cundall <harry@cundalls.com>
2022-08-30 15:35:01 +0100

    index view hardcoded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-require 'faker'
-
-50.times do
-  friend = Friend.create!(
-    user_id: 1,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    categories: %w[sports music funeral family date travel].sample,
-    age: Faker::Number.between(from: 18, to: 70),
-    location: Faker::Address.state,
-    daily_rate: Faker::Number.between(from: 10, to: 100),
-    pronouns: 'mr',
-    bio: 'thew jwefejwk jewfknfjkew fkjwenfgj wekjfbjkew fjkwe fjkwfe fjkew f'
-  )
-  puts "#{friend.id} created!"
-end
