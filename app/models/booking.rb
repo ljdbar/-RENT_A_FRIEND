15822f03376d5ad9b33f1b52e233c39b1fdf7312
Harry Cundall <harry@cundalls.com>
2022-08-30 11:00:39 +0100

    validations almost done for booking, need to add some more(see comment) HARRY

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :friend
   belongs_to :user
+  validates :start_date, presence: true, uniqueness: true, comparison: { greater_than: :end_date }
+  validates :end_date, presence: true, uniqueness: true
+  validates :total_price, presence: true, numericality: { only_float: true }
 end
+
+#Add START and END dates cannot be in the past
