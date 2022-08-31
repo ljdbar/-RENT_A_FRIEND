ed21f67cc95925692774355a545d1beba797a324
Harry Cundall <harry@cundalls.com>
2022-08-31 14:29:20 +0100

    cool

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -14,14 +14,19 @@ class FriendsController < ApplicationController
 
   def create
     @friend = Friend.new(friend_params)
-    @friend.save
-    redirect_to friend_path(@friend)
-    raise
+    @friend.user = current_user
+   if  @friend.save
+      redirect_to friend_path(@friend)
+   else
+    @categories = %w[sports music funeral family date travel]
+    render :new, status: :unprocessable_entity
+   end
   end
 
   private
 
   def friend_params
-    params.require(:friend).permit(:first_name, :last_name, :categories, :age, :location, :pronouns, :bio, :daily_rate, :user_id)
+    params.require(:friend).permit(:first_name, :last_name, :age, :location, :pronouns, :bio, :daily_rate, :user_id, :categories)
+    #change :catagories to catagories: []
   end
 end
