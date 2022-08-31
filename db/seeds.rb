fda8ffc7e00ccb9da05297bb9919b6b94e574be7
Jason Armstrong <jasnarmstrng@gmail.com>
2022-08-31 12:32:09 +0100

    Updated seed for user and friend

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,56 @@
 
 require 'faker'
 
-10.times do
-  friend = Friend.create!(
-    user_id: 1,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+12.times do
+  name = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
+  email = Faker::Internet.email(name: name.to_s, separators: ['-'], domain: 'gmail')
+  username = email.split('@')[0]
+  user = User.create!(username: username, email: email, password: '123456')
+  puts "#{user.id} created!"
+end
+
+addresses = ['201, 217 Commercial Rd, London E1 2BT',
+             'Unit 25, The Concourse Liverpool Street Station, London EC2M 7PY',
+             '432, 436 Bethnal Green Rd, London E2 0DJ',
+             '6/7 Holborn, London EC1N 2LL',
+             '1A Ludgate Hill, London EC4M 7AA',
+             '36/42 Kingsland High St, London E8 2JP',
+             '251/252 Upper St, London N1 1RU',
+             '359/363 Mare St, London E8 1HY',
+             '102, 106 The Hwy, London E1W 2BX',
+             '24, Cambridge Circus, 84-86 Charing Cross Rd, London WC2H 8AA',
+             '291B Oxford St, London W1C 2DT',
+             '25/27 Shaftesbury Ave, London W1D 7EQ']
+index = 0
+
+User.all.each do |user|
+  friend = Friend.create(
+    user: user,
+    first_name: user.username.split('-')[0].titleize,
+    last_name: user.username.split('-')[1].titleize,
     categories: %w[sports music funeral family date travel].sample,
     age: Faker::Number.between(from: 18, to: 70),
-    location: Faker::Address.state,
+    location: addresses[index],
     daily_rate: Faker::Number.decimal(l_digits: 2),
-    pronouns: 'mr',
-    bio: 'thew jwefejwk jewfknfjkew fkjwenfgj wekjfbjkew fjkwe fjkwfe fjkew f'
+    pronouns: ['He/Him', 'She/Her', 'They/Them'].sample,
+    bio: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 50)
   )
   puts "#{friend.id} created!"
+  index += 1
 end
+# 10.times do
+#   friend = Friend.create!(
+#     user_id: 1,
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     categories: %w[sports music funeral family date travel].sample,
+#     age: Faker::Number.between(from: 18, to: 70),
+#     location: Faker::Address.state,
+#     daily_rate: Faker::Number.decimal(l_digits: 2),
+#     pronouns: ['He/Him', 'She/Her', 'They/Them'].sample,
+#     bio: 'thew jwefejwk jewfknfjkew fkjwenfgj wekjfbjkew fjkwe fjkwfe fjkew f'
+#   )
+#   puts "#{friend.id} created!"
+# end
 
 # https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png
