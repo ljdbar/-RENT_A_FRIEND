6d89d72d08f8e4ac05d4062c15a5cfb8b72c4463
Harry Cundall <harry@cundalls.com>
2022-09-02 14:53:55 +0100

    made some small tweaks

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -40,6 +40,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 100 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2400 })
   };
 }
