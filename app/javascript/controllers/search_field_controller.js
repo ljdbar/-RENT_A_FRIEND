ec988690f4209bfcffecfb1e3b65ba8f4cba720f
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-09 20:57:42 +0100

    created event listener for search icon

--- app/javascript/controllers/search_field_controller.js
+++ app/javascript/controllers/search_field_controller.js
@@ -4,5 +4,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     console.log('hello search field')
+
+    const searchIcon = document.querySelector('.search-icon');
+    const searchField = document.querySelector(".search-field");
+    const searchBtn = document.querySelector(".search-btn");
+
+    searchIcon.addEventListener('click', (event) => {
+      searchIcon.classList.toggle('d-none')
+      searchField.classList.toggle('d-none')
+      searchBtn.classList.toggle('d-none')
+    })
   }
 }
