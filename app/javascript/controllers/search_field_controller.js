cb3ca2cc28f6f6cd712062b92f31f35ff0962d38
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-09 21:05:03 +0100

    added a search bar hiding btn

--- app/javascript/controllers/search_field_controller.js
+++ app/javascript/controllers/search_field_controller.js
@@ -8,11 +8,20 @@ export default class extends Controller {
     const searchIcon = document.querySelector('.search-icon');
     const searchField = document.querySelector(".search-field");
     const searchBtn = document.querySelector(".search-btn");
+    const searchHide = document.querySelector(".search-hide");
 
     searchIcon.addEventListener('click', (event) => {
       searchIcon.classList.toggle('d-none')
       searchField.classList.toggle('d-none')
       searchBtn.classList.toggle('d-none')
+      searchHide.classList.toggle('d-none')
+    })
+
+    searchHide.addEventListener('click', (event) => {
+      searchIcon.classList.toggle('d-none')
+      searchField.classList.toggle('d-none')
+      searchBtn.classList.toggle('d-none')
+      searchHide.classList.toggle('d-none')
     })
   }
 }
