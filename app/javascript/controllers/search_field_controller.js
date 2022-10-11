28987f8547c393a08dd6b42a3bebc07e20d0dfba
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-11 15:22:57 +0100

    added animated search box

--- app/javascript/controllers/search_field_controller.js
+++ app/javascript/controllers/search_field_controller.js
@@ -3,25 +3,42 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-field"
 export default class extends Controller {
   connect() {
-    console.log('hello search field')
+    console.log('hello search field');
 
-    const searchIcon = document.querySelector('.search-icon');
-    const searchField = document.querySelector(".search-field");
-    const searchBtn = document.querySelector(".search-btn");
-    const searchHide = document.querySelector(".search-hide");
+    const searchBtn = document.querySelector('.search-btn-a');
+    const cancelBtn = document.querySelector('.cancel-btn');
+    const searchBox = document.querySelector('.search-box');
 
-    searchIcon.addEventListener('click', (event) => {
-      searchIcon.classList.toggle('d-none')
-      searchField.classList.toggle('d-none')
-      searchBtn.classList.toggle('d-none')
-      searchHide.classList.toggle('d-none')
+    searchBtn.addEventListener('click', (event) => {
+      console.log('hello search bar');
+      searchBox.classList.add('active');
     })
 
-    searchHide.addEventListener('click', (event) => {
-      searchIcon.classList.toggle('d-none')
-      searchField.classList.toggle('d-none')
-      searchBtn.classList.toggle('d-none')
-      searchHide.classList.toggle('d-none')
+    cancelBtn.addEventListener('click', (event) => {
+      console.log('byebye search bar');
+      searchBox.classList.remove('active');
     })
+
+
+    // const searchIcon = document.querySelector('.search-icon');
+    // const searchBar = document.querySelector('.search-bar');
+    // const searchField = document.querySelector(".search-field");
+    // const searchBtn = document.querySelector(".search-btn");
+    // const searchHide = document.querySelector(".search-hide");
+
+    // searchIcon.addEventListener('click', (event) => {
+    //   searchIcon.classList.toggle('disappear')
+    //   searchBar.classList.toggle('appear')
+      // searchField.classList.toggle('d-none')
+      // searchBtn.classList.toggle('d-none')
+      // searchHide.classList.toggle('d-none')
+    // })
+
+    // searchHide.addEventListener('click', (event) => {
+    //   searchIcon.classList.toggle('d-none')
+      // searchField.classList.toggle('d-none')
+      // searchBtn.classList.toggle('d-none')
+      // searchHide.classList.toggle('d-none')
+    // })
   }
 }
